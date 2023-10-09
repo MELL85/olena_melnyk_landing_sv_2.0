41ec8d10cebd353efd62b1a79767fdd67454e3bc
Yevhen <melnyk_evgenij@icloud.com>
2023-10-09 21:20:43 +0300

    all styles done

--- js/carousel-swiper.js
+++ js/carousel-swiper.js
@@ -1,23 +1,20 @@
 (() => {
     const swiper = new Swiper('.swiper', {
-        // Optional parameters
-        direction: 'vertical',
-        loop: true,
 
-        // If we need pagination
-        pagination: {
-            el: '.swiper-pagination',
-        },
+        effect: "cards",
+        grabCursor: true,
 
-        // Navigation arrows
+        // pagination: {
+        //     el: '.swiper-pagination',
+        // },
+
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
 
-        // And if we need scrollbar
-        scrollbar: {
-            el: '.swiper-scrollbar',
-        },
+        // scrollbar: {
+        //     el: '.swiper-scrollbar',
+        // },
     });
 })();
