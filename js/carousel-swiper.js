79c939eecba013371af280044d95d604aa8ff822
Yevhen <melnyk_evgenij@icloud.com>
2023-10-16 14:22:27 +0300

    deleted comments and structured the media screen

--- js/carousel-swiper.js
+++ js/carousel-swiper.js
@@ -4,17 +4,9 @@
         effect: "cards",
         grabCursor: true,
 
-        // pagination: {
-        //     el: '.swiper-pagination',
-        // },
-
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
-
-        // scrollbar: {
-        //     el: '.swiper-scrollbar',
-        // },
     });
 })();
